2bb205455a2778a37fa5213d8ae6f9baae7c444d
Angelina Ma <angelina890308@gmail.com>
2021-02-19 00:21:11 -0800

    update view all function to display department name, manager name instead of id

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function startPrompt() {
         viewAll("ROLE");
         break;
       case "View all departments":
-        viewAll("department");
+        viewAll("DEPARTMENT");
         break;
       case "add a department":
         addNewDepartment();
@@ -83,7 +83,22 @@ function startPrompt() {
 
 
 const viewAll = (table) => {
-  const query = `SELECT * FROM ${table}`;
+  // const query = `SELECT * FROM ${table}`;
+  let query;
+  if (table === "DEPARTMENT") {
+    query = `SELECT * FROM DEPARTMENT`;
+  } else if (table === "ROLE") {
+    query = `SELECT R.id AS id, title, salary, D.name AS department
+    FROM ROLE AS R LEFT JOIN DEPARTMENT AS D
+    ON R.department_id = D.id;`;
+  } else {//employee
+    query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
+    R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
+    FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
+    LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+    LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id;`;
+
+  }
   connection.query(query, (err, res) => {
     if (err) throw err;
     console.table(res);
